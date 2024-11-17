ad661db4cd31ee33d0e5277a9b71dc02bf297569
oddpharmacy <107352832+oddpharmacy@users.noreply.github.com>
2024-11-18 05:30:17 +0800

    feat: create cypress config file

---
+++ cypress.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "cypress";
+ 
+export default defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    specPattern: "cypress/e2e/*.cy.tsx",
+    supportFile: "cypress/support/*.ts"
+  },
+  component: {
+    devServer: {
+        framework: "next",
+        bundler: "webpack",
+    },
+    specPattern: "cypress/component/*.cy.tsx",
+    supportFile: false,
+  },
+})
