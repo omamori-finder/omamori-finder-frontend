850c36853903451b3541d961aa8a17c16f67f320
oddpharmacy <107352832+oddpharmacy@users.noreply.github.com>
2025-01-22 04:04:09 +0800

    chore: add Cardfeed to the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
 import Map from "./_components/Map";
+import Cardfeed from "./_components/Cardfeed";
+
 export default function Home() {
   return (
     <div>
       <Map />
+      <Cardfeed />
     </div>
-  )
+  );
 }
