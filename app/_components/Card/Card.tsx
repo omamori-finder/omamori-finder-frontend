8f249ff1a0e865ca1c9c9f39e4ee8fd8e996c41e
oddpharmacy <107352832+oddpharmacy@users.noreply.github.com>
2025-01-22 04:03:10 +0800

    chore: cleaned up Card component

--- app/_components/Card/Card.tsx
+++ app/_components/Card/Card.tsx
@@ -1,24 +1,25 @@
 import Image from "next/image";
+import Link from "next/link";
 
 type CardProps = {
-  image: string;
+  //image: string;
   shrineName: string;
   tag: string;
-  link: string;
+  //link: string;
 };
 
-const Card = ({ image, shrineName, tag, link }: CardProps) => {
+const Card = ({ shrineName, tag }: CardProps) => {
   return (
-    <li className="relative w-1/4">
+    <li className="relative">
       <article>
         <div>
           <div className="aspect-square relative overflow-hidden rounded-lg">
             <Image
-              src={image}
-              alt={shrineName}
+              src="/placeholder_omamori.jpg"
+              alt={shrineName || "Shrine"}
               loading="lazy"
               className="w-full h-full object-cover cursor-pointer rounded-lg"
-              layout="fill"
+              fill
             />
           </div>
           <div>
@@ -26,9 +27,9 @@ const Card = ({ image, shrineName, tag, link }: CardProps) => {
               <p className="flex h-5 order-1 px-3.5 py-0 border border-solid rounded-lg border-[var(--card-border-color)] items-center justify-center md:rounded-2xl md:h-7 md:min-w-24">
                 {tag}
               </p>
-              <a href={link} className="order-2 cursor-pointer">
+              <Link href="/" className="order-2 cursor-pointer">
                 ↗ Link
-              </a>
+              </Link>
             </div>
             <h3 className="mt-2.5">{shrineName}</h3>
           </div>
@@ -37,36 +38,5 @@ const Card = ({ image, shrineName, tag, link }: CardProps) => {
     </li>
   );
 };
-
-// const Test = () => {
-//   return (
-//     <li className="relative w-1/4">
-//       <article>
-//         <div>
-//           <div className="aspect-square relative overflow-hidden rounded-lg">
-//             <Image
-//               src="/omamori.jpg"
-//               alt="hi"
-//               loading="lazy"
-//               className="w-full h-full object-cover cursor-pointer"
-//               layout="fill"
-//             />
-//           </div>
-//           <div>
-//             <div className="mt-3.5 flex flex-wrap justify-between relative select-none">
-//               <p className="flex h-5 order-1 px-3.5 py-0 border border-solid rounded-lg border-[var(--card-border-color)] items-center justify-center md:rounded-2xl md:h-7 md:min-w-24">
-//                 Good Luck
-//               </p>
-//               <a href="/" className="order-2 cursor-pointer">
-//                 ↗ Link
-//               </a>
-//             </div>
-//             <h3 className="mt-2.5">Meiji Jingu</h3>
-//           </div>
-//         </div>
-//       </article>
-//     </li>
-//   );
-// };
 
 export default Card;
