ce1574f5fe94d533dc195d699e5cc12e4f0af7c1
oddpharmacy <107352832+oddpharmacy@users.noreply.github.com>
2025-01-22 04:03:52 +0800

    feat: create a feed of omamori cards

---
+++ app/_components/Cardfeed/Cardfeed.tsx
@@ -0,0 +1,26 @@
+import Card from "../Card/Card";
+
+const Cardfeed = () => {
+  // Only for testing
+  const testData = [
+    { key: 1, shrineName: "Meiji Jingu", tag: "Good Luck" },
+    { key: 2, shrineName: "Sensoji", tag: "Marriage" },
+    { key: 3, shrineName: "Ryoanji", tag: "Good Luck" },
+    { key: 4, shrineName: "Ryoanji", tag: "Traffic" },
+    { key: 5, shrineName: "Ryoanji", tag: "Health" },
+  ];
+
+  return (
+    <ul className="list-none pl-0 m-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+      {testData.map((omamori) => (
+        <Card
+          key={omamori.key}
+          shrineName={omamori.shrineName}
+          tag={omamori.tag}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default Cardfeed;
