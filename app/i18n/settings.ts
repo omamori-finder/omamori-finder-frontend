da9d4e06c8bac1fc5e256722cbc5857bf3f50926
oddpharmacy <107352832+oddpharmacy@users.noreply.github.com>
2025-03-17 03:56:45 +0800

    feat: set i18n settings

---
+++ app/i18n/settings.ts
@@ -0,0 +1,18 @@
+export const fallbackLng = "en";
+export const languages = [fallbackLng, "jp"];
+export const defaultNS = "translation";
+export const cookieName = "i18next";
+
+export function getOptions(
+  lng = fallbackLng,
+  ns: string | string[] = defaultNS
+) {
+  return {
+    supportedLngs: languages,
+    fallbackLng,
+    lng,
+    fallbackNS: defaultNS,
+    defaultNS,
+    ns,
+  };
+}
