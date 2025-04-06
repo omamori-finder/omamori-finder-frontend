198d36925d77f041fe2d0f17e7dceb2e67209c36
oddpharmacy <107352832+oddpharmacy@users.noreply.github.com>
2025-04-06 22:17:13 +0800

    docs: add clarifying comments, change one-liners and remove semi-colons where possible

--- middleware.ts
+++ middleware.ts
@@ -5,27 +5,36 @@ import { fallbackLng, languages, cookieName } from "./app/i18n/settings";
 acceptLanguage.languages(languages);
 
 export const config = {
-  // matcher: '/:lng*'
   matcher: [
     "/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js|site.webmanifest).*)",
   ],
 };
 
 export function middleware(req: NextRequest) {
-  if (
-    req.nextUrl.pathname.indexOf("icon") > -1 ||
-    req.nextUrl.pathname.indexOf("chrome") > -1
-  )
-    return NextResponse.next();
+  // Exclude paths with icon/chrome
+  // Checks if substring includes icon/chrome, for eg if checking for icon it will return positive if found and -1 if not found
+  const isIconPath = req.nextUrl.pathname.indexOf("icon") > -1
+  const isChromePath = req.nextUrl.pathname.indexOf("chrome") > -1
+  if (isIconPath || isChromePath) {
+    return NextResponse.next()
+  }
+
   let lng: string | undefined | null;
 
   // Check and use cookies if available
-  if (req.cookies.has(cookieName))
-    lng = acceptLanguage.get(req.cookies.get(cookieName)?.value);
-  if (!lng) lng = acceptLanguage.get(req.headers.get("Accept-Language"));
-  if (!lng) lng = fallbackLng;
+  if (req.cookies.has(cookieName)) {
+    lng = acceptLanguage.get(req.cookies.get(cookieName)?.value)
+  }
+  // No cookie = fall back to header
+  if (!lng) {
+    lng = acceptLanguage.get(req.headers.get("Accept-Language"))
+  }
+  // No cookie and no header
+  if (!lng) {
+    lng = fallbackLng
+  }
 
-  // Redirect if lng in path is not supported
+  // Redirect if language in path is not supported
   if (
     !languages.some((loc) => req.nextUrl.pathname.startsWith(`/${loc}`)) &&
     !req.nextUrl.pathname.startsWith("/_next")
@@ -35,15 +44,20 @@ export function middleware(req: NextRequest) {
     );
   }
 
-  if (req.headers.has("referer")) {
-    const refererUrl = new URL(req.headers.get("referer") || "");
+  // Detect language from referer url (url of page the user came from before arriving on the current page)
+  const hasRefererHeader = req.headers.has("referer")
+  if (hasRefererHeader) {
+    const refererUrl = new URL(req.headers.get("referer") || "")
     const lngInReferer = languages.find((l) =>
       refererUrl.pathname.startsWith(`/${l}`)
     );
-    const response = NextResponse.next();
-    if (lngInReferer) response.cookies.set(cookieName, lngInReferer);
-    return response;
+    const response = NextResponse.next()
+    // Store language in cookie
+    if (lngInReferer) {
+      response.cookies.set(cookieName, lngInReferer)
+    }
+    return response
   }
 
-  return NextResponse.next();
+  return NextResponse.next()
 }
