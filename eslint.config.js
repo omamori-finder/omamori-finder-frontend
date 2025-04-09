12cfa2b4eac004643264e87d12bfc3640471d898
Anissa3005 <anissa.chadouli@gmail.com>
2025-04-09 10:00:10 +0900

    chore: add styling rules to eslint.config file

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,19 @@
-
-import eslint from '@eslint/js'
-import tseslint from 'typescript-eslint'
+import eslint from "@eslint/js"
+import tseslint from "typescript-eslint"
+import stylistic from "@stylistic/eslint-plugin"
+import stylisticJsx from "@stylistic/eslint-plugin-jsx"
 
 export default tseslint.config(
     eslint.configs.recommended,
     tseslint.configs.recommended,
     {
-        ignores: [".next/**"]
+        plugins: {
+            "@stylistic": stylistic,
+            "@stylistic/jsx": stylisticJsx
+        }
+    },
+    {
+        ignores: [".next/**", "next-env.d.ts"]
     },
     // Linting rules for .ts/js files
     {
@@ -16,7 +23,7 @@ export default tseslint.config(
             "@typescript-eslint/no-confusing-non-null-assertion": "error",
             // eslint rules
             // Turning it of so we can use this rule from eslint-ts
-            'no-unused-vars': 'off',
+            "no-unused-vars": "off",
             "arrow-body-style": ["error", "always"],
             "curly": "error",
             "dot-notation": "error",
@@ -29,5 +36,43 @@ export default tseslint.config(
             "yoda": "error"
         }
 
+    },
+    // Style rules for .ts/js files
+    {
+        files: ["cypress", "**/*.{ts,js,tsx}"],
+        rules: {
+            "@stylistic/semi": ["error", "never"],
+            "@stylistic/array-bracket-spacing": ["error", "never"],
+            "@stylistic/arrow-parens": ["error", "always"],
+            "@stylistic/arrow-spacing": "error",
+            "@stylistic/brace-style": "error",
+            "@stylistic/comma-dangle": ["error", "never"],
+            "@stylistic/comma-spacing": [
+                "error", 
+                { "before": false, "after": true }
+            ],
+            "@stylistic/comma-style": ["error", "last"],
+            "@stylistic/curly-newline": ["error", "always"],
+            "@stylistic/function-call-spacing": ["error", "never"],
+            "@stylistic/function-paren-newline": ["error", "consistent"],
+            "@stylistic/implicit-arrow-linebreak": ["error", "beside"],
+            "@stylistic/indent": ["error", 4],
+            "@stylistic/key-spacing": ["error", { "beforeColon": false }],
+            "@stylistic/keyword-spacing": ["error", { "before": true }],
+            "@stylistic/max-len": ["error", { "code": 130,
+                "ignoreRegExpLiterals": true,
+                "ignorePattern": "^\\s*className\\s*[:=]\\s*`?.*"
+            }],
+            "@stylistic/multiline-comment-style": ["error", "starred-block"],
+            "@stylistic/no-multi-spaces": "error",
+            "@stylistic/quotes": ["error", "double"],
+            // jsx styling rules
+            "@stylistic/jsx/jsx-closing-bracket-location": [1, "tag-aligned"],
+            "@stylistic/jsx/jsx-curly-newline": ["error", {multiline: "consistent", singleline: "consistent"}],
+            "@stylistic/jsx/jsx-equals-spacing": ["error", "never"],
+            "@stylistic/jsx/jsx-first-prop-new-line": ["error", "multiline"],
+            "@stylistic/jsx/jsx-pascal-case": ["error"]
+        }
+
     }
-);
+)
